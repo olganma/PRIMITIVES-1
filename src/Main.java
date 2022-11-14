e0748823143cca945eccd358403b6396872c3398
Maevskaya Olga <olganma@gmail.com>
2022-11-14 17:16:48 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 200; // цена купленного билета
+        int miles;      // начисленные мили
+
+        miles = price / 20;
+        System.out.println("За купленный билет начислено - " + miles + " миль");
+    }
+}
